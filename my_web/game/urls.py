35e3587f8e3ca2a1d68bd0b155678382f278e5f1
plokmit <savi4eva.niu@yandex.ru>
2023-04-21 01:18:56 +0300

    Добавлены виды ставок, коэффициенты для выигрыша, поля для выбора чисел

--- my_web/game/urls.py
+++ my_web/game/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.start, name='game'),
-    path('new_game/', views.one_game, name='new_game')
+    path('new_game/', views.new_game, name='new_game'),
+    path('new_game/result/', views.result, name='result')
 ]
--- my_web/game/views.py
+++ my_web/game/views.py
@@ -2,30 +2,73 @@ from random import randint
 from django.shortcuts import render, redirect
 from users.models import User, Payment
 
+choice_bet = 0
 def start(request):
     template_name = 'game.html'
     return render(request, template_name)
-def one_game(request):
-    red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+def new_game(request):
+    template_name = 'choice.html'
+
+    global choice_bet
+    choice_bet = int(request.POST.get('choice_bet'))
+
+    data = {
+        'choice_bet': choice_bet
+    }
+    return render(request, template_name, data)
+
+def result(request):
     template_name = 'result.html'
-    number = int(request.POST.get('number'))
-    bet = int(request.POST.get('bet'))
+    global choice_bet
+
+    red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+    coefficients = {
+        1: 35,
+        2: 17,
+        3: 11,
+        4: 8,
+        5: 1,
+        6: 1
+    }
+
     username = request.user.username
     row = User.objects.get(username=username)
+    number = request.POST.getlist("number")
+    print(number)
+    number = [int(i) for i in number]
+    bet = int(request.POST.get('bet'))
+    print(number)
+
+    if 0 < choice_bet < 5:
+        choice_bet = len(set(number))
+    elif choice_bet == 5:
+        if 1 in number:
+            number = red
+        if 2 in number:
+            number = [i for i in range(36) if i not in red]
+
+    elif choice_bet == 6:
+        if 1 in number:
+            number = [_ for _ in range(1, 36, 2)]
+        if 2 in number:
+            number = [_ for _ in range(2, 37, 2)]
+
     if bet > row.payment.balance:
         return redirect('game')
     res_of_number = randint(0, 36)
     res_of_number = 5
-    if number in red:
+
+    if res_of_number in red:
         color = 'красное'
     else:
         color = 'черное'
-    if request.user.is_authenticated:
 
-        print(row.payment.balance)
-        if number == res_of_number:
+    if request.user.is_authenticated:
+        print(choice_bet)
+        if res_of_number in number:
             message = 'Поздравляем! Вы выиграли! '
-            row.payment.balance += bet*35
+            row.payment.balance += bet*coefficients[choice_bet]
+
         else:
             message = 'Сожалеем, вы проиграли'
             row.payment.balance -= bet
@@ -38,5 +81,7 @@ def one_game(request):
         'color': color,
         'message': message,
         'balance': balance,
+        'bet': bet,
     }
+
     return render(request, template_name, data)
